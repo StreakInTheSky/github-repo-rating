b6d17d1a6ceb37a2a2d131e45d35105cd94fbe14
StreakInTheSky <streakinthesky@gmail.com>
2017-04-22 21:20:13 -0700

    Create reducer and store

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
 
+import * as actions from './actions/index';
+import store from './store';
+
+// store.dispatch(actions.addRepository('joe'));
+// console.log(store.getState()); // Logs [{ name: 'joe', rating: null}]
+
+window.actions = actions;
+window.store = store;
